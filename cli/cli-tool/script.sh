be20628fdd63bd8b83639bb32f8e2497e84af0e6
ntua-el19604 <el19604@mail.ntua.gr>
2023-02-05 20:40:39 +0200

    Added CLI shell script

---
+++ cli/cli-tool/script.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo -e "HEALTHCHECK TEST \n"
+sleep 0.5
+se2226 healthcheck --format json
+echo "Press any key to continue..."
+read x
+
+echo -e "QUESTIONNAIRE TEST \n"
+sleep 0.5
+se2226 questionnaire --questionnaire_id QQ001 --format json
+echo "Press any key to continue..."
+read x
+
+echo -e "DOANSWER TEST \n"
+sleep 0.5
+se2226 doanswer --questionnaire_id QQ001 --question_id  --session_id  --option_id --format json
+echo "Press any key to continue..."
+read x
