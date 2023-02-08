3891504f1eb9e0e89eb04e720d48520509fba4ed
ntua-el19610 <115106424+ntua-el19610@users.noreply.github.com>
2023-02-08 21:52:00 +0200

    fixed Docker configuration

---
+++ api-backend/config-docker.js
@@ -0,0 +1,34 @@
+/*
+            intelliQ
+    API (Backend) Application
+        Configuration File
+*/
+
+const config = {
+    http: {
+        enabled: false,
+        host: "127.0.0.1",
+        port: 9102
+
+    },
+
+    https: {
+        enabled: true,
+        host: "0.0.0.0",
+        port: 9103,
+        ssl: {
+            key: "ssl/key.pem",
+            cert: "ssl/cert.pem"
+        }
+    },
+
+    mariadb: {
+        host: "intelliq-database",
+        port: 3306,
+        user: "intelliq",
+		password: "intelliq",
+        database: "intelliq"
+    }
+};
+
+module.exports = config;
