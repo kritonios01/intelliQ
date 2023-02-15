b053081ec5998a47f2af9a3b44ac14db1a7111a9
ntua-el19198 <115098148+ntua-el19198@users.noreply.github.com>
2023-02-15 03:17:10 +0200

    Updated question.html and question.js

--- frontend/assets/javascript/question.js
+++ frontend/assets/javascript/question.js
@@ -1,18 +1,84 @@
+//---Initializing variables
+
 const queryString = window.location.search;
 const urlParam = new URLSearchParams(queryString);
 const questionnaireID = urlParam.get('questionnaireID');
 const session = urlParam.get('session');
 const questionURL = `https://api.intelliq.site/intelliq_api/questionnaire/${questionnaireID}`;
-console.log(questionnaireID);
-console.log(session);
 const question = document.getElementById("question");
+let qdata;
 
+//---GET all the data we want from Endpoints----
 
-async function getapi(url){
+getquestions(questionURL);
+
+async function getquestions(url){
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
-    console.log(data.questions[0].qtext);
-    question.innerText = data.questions[0].qtext;
+    qdata = data;
+    getoptions();
 }
-getapi(questionURL);
+async function getoptions(){
+    for(let index = 0;index < qdata.questions.length;index++){
+        const res = await fetch(`https://api.intelliq.site/intelliq_api/question/${questionnaireID}/${qdata.questions[index].qID}`);
+        const data = await res.json();
+        qdata.questions[index].options = data.options;
+    }
+    init()
+}
+//---Starting Questionnaire
+
+function init(){
+    let qcounter =0;
+    iterate(3);
+}
+function iterate(id){
+    question.innerText=qdata.questions[id].qtext;
+    const showoptions = document.getElementById("option-container");
+    for (let index = 0; index < qdata.questions[id].options.length; index++) {
+        showoptions.innerHTML += `
+        <input type="radio" name="questionID" id="${index}" value="${qdata.questions[id].options[index].optID}">
+        <label for="${index}">${qdata.questions[id].options[index].opttxt}</label><br>
+        `
+    }
+}
+let selected;
+let nextqID;
+const myform = document.getElementById('myform');
+myform.addEventListener('submit',function(e){
+    e.preventDefault();
+    const selectedOptions = document.getElementsByName('questionID');
+    console.log(selectedOptions);
+    for(var radio of selectedOptions){
+        if(radio.checked){
+            selected=radio.value;
+            nextqID=qdata.questions[3].options[radio.id].nextqID;
+            console.log(selected);
+            console.log(nextqID);
+            break;
+        }
+    }
+})
+/*getNewQuestion
+->Get asnwer
+->Store answer
+->Give next question based on nextqid
+->If nextqid == null stop
+*/
+function getNewQuestion(id){
+    const myform = document.getElementById('myform');
+myform.addEventListener('submit',function(e){
+    e.preventDefault();
+    const selectedOptions = document.getElementsByName('questionID');
+    console.log(selectedOptions);
+    for(var radio of selectedOptions){
+        if(radio.checked){
+            selected=radio.value;
+            nextqID=qdata.questions[3].options[radio.id].nextqID;
+            console.log(selected);
+            console.log(nextqID);
+            break;
+        }
+    }
+})
+}
