8d49f3d3525fafe89ee3d284f92eb5dee70f1672
ntua-el19018 <el19018@mail.ntua.gr>
2023-02-18 03:27:36 +0200

    CLI setup file change

--- cli/setup.py
+++ cli/setup.py
@@ -4,7 +4,7 @@ setup(
 	name='intelliQ-CLI',
 	version='1.0',
 	py_modules=['cli'],
-	install_requires=['click','requests','pandas','tabulate', 'os', 'tempfile', 'pytest'],
+	install_requires=['click','requests','pandas','tabulate', 'pytest'],
 	entry_points={
 		'console_scripts': [
 			'se2226=cli:main'
