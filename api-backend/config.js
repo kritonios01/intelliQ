313ab0c1fd49e76c13439715a27af6a720a2166f
Ioannis Dressos <115629281+ntua-el19608@users.noreply.github.com>
2023-02-18 20:09:27 +0200

    Add some configuration commentary

--- api-backend/config.js
+++ api-backend/config.js
@@ -5,6 +5,7 @@
 */
 
 const config = {
+    // HTTP server configuration
     http: {
         enabled: true,
         host: "127.0.0.1",
@@ -12,6 +13,7 @@ const config = {
 
     },
 
+    // HTTPS server configuration
     https: {
         enabled: false,
         host: "127.0.0.1",
@@ -22,15 +24,21 @@ const config = {
         }
     },
 
+    // Cross-Origin Resource Sharing configuration
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     cors: {
         origin: /^(http:\/\/|https:\/\/)?(intelliq.site|localhost)\/?$/,
         optionsSuccessStatus: 200
     },
 
+    // Documentation configuration
+    // Available at /docs when enabled
     docs: {
         enabled: true
     },
 
+    // MariaDB configuration
+    // Leave password field empty for users witout password
     mariadb: {
         host: "127.0.0.1",
         port: 3306,
--- frontend/js/config/config.js
+++ frontend/js/config/config.js
@@ -1,5 +1,6 @@
 const config = {
     api: {
+        // API requests will be made using this base URL
         base_url: "https://api.intelliq.site/intelliq_api"
     }
 }
